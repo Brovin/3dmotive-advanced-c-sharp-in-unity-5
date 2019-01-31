152312199e9581f75700439c255e333949ea8a9f
Boris <45284853+Brovin@users.noreply.github.com>
2019-02-01 02:16:07 +0300

    update

---
+++ Assets/Scripts/Orbiter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbiter : MonoBehaviour
+{
+    public Transform Pilov = null;
+    private Transform ThisTransform = null;
+    private Quaternion DestRot = Quaternion.identity;
+    public float PivotDistance = 5f;
+    public float RotSpeed = 10f;
+    private float RotX = 0f;
+    private float RotY = 0f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        ThisTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RotateTo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTo : MonoBehaviour
+{
+    private Transform ThisTransform = null;
+    public float RotSpeed = 90f;
+    public Transform Target = null;
+    void Awake()
+    {
+        ThisTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        Quaternion DestRot = Quaternion.LookRotation(Target.position - ThisTransform.position, Vector3.up);
+        ThisTransform.rotation = Quaternion.RotateTowards(ThisTransform.rotation, DestRot, RotSpeed * Time.deltaTime);
+        //ThisTransform.rotation *= Quaternion.AngleAxis(RotSpeed * Time.deltaTime, Vector3.up);
+    }
+}
